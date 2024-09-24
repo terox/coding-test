b7a7d531dd08daacae0724b27912e32c02912d1c
David Pérez Terol <davidtxnet@gmail.com>
2024-09-24 19:45:30 +0200

    Remove abstraction layer to simply the design

--- src/Core/Discount/Domain/Discounts.php
+++ src/Core/Discount/Domain/Discounts.php
@@ -4,8 +4,20 @@ declare(strict_types=1);
 
 namespace Teamleader\Discounts\Core\Discount\Domain;
 
-use Teamleader\Discounts\Core\Shared\Domain\DiscountCollection;
+use Teamleader\Discounts\Shared\Domain\CollectionBase;
 
-final class Discounts extends DiscountCollection
+final class Discounts extends CollectionBase
 {
+    public function getOneOrNull(int $index): ?Discount
+    {
+        return $this->items[$index] ?? null;
+    }
+
+    /**
+     * @return Discount[]
+     */
+    public function toArray(): array
+    {
+        return $this->items;
+    }
 }
