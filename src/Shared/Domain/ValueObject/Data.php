98ca1d0be984a56236170234d58f07216105b127
David Pérez Terol <davidtxnet@gmail.com>
2024-09-25 13:20:47 +0200

    Add access method to Data VO

--- src/Shared/Domain/ValueObject/Data.php
+++ src/Shared/Domain/ValueObject/Data.php
@@ -13,4 +13,33 @@ class Data
         private readonly array $data
     ) {
     }
+
+    public function hasKey(string $key): bool
+    {
+        return array_key_exists($key, $this->data);
+    }
+
+    public function get(string $key): mixed
+    {
+        if(!$this->hasKey($key)) {
+            return null;
+        }
+
+        return $this->data[$key];
+    }
+
+    public function isKeyType(string $key, string $type): bool
+    {
+        if(!$this->hasKey($key)) {
+            return false;
+        }
+
+        $value = $this->get($key);
+
+        return match ($type) {
+            'int'   => is_int($value),
+            'float' => is_float($value),
+            default => false,
+        };
+    }
 }
