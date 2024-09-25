a01b5d530bceadafe5246aacf292f706659b2779
David Pérez Terol <davidtxnet@gmail.com>
2024-09-25 11:31:41 +0200

    Change class type and visibility methods

--- tests/Core/Discount/DiscountModuleTestCase.php
+++ tests/Core/Discount/DiscountModuleTestCase.php
@@ -9,18 +9,18 @@ use Teamleader\Discounts\Core\Discount\Domain\DiscountRepository;
 use Teamleader\Discounts\Core\Discount\Domain\Discounts;
 use Teamleader\Discounts\Tests\Shared\Infrastructure\Testing\PHPUnit\TestCase;
 
-class DiscountModuleTestCase extends TestCase
+abstract class DiscountModuleTestCase extends TestCase
 {
     private DiscountRepository|MockInterface $repository;
 
-    public function repositoryReturnNextDiscounts(Discounts $discounts): void
+    protected function repositoryReturnNextDiscounts(Discounts $discounts): void
     {
         $this->repository()
             ->expects('findAll')
             ->andReturn($discounts);
     }
 
-    public function repository(): DiscountRepository|MockInterface
+    protected function repository(): DiscountRepository|MockInterface
     {
         return $this->repository ??= $this->mock(DiscountRepository::class);
     }
