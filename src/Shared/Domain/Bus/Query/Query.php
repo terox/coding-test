ca28abe1936a1ba118d4cfca26dda8a6f39d9636
David Pérez Terol <davidtxnet@gmail.com>
2024-09-25 10:16:33 +0200

    Add QueryBus

---
+++ src/Shared/Domain/Bus/Query/Query.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Teamleader\Discounts\Shared\Domain\Bus\Query;
+
+interface Query
+{
+
+}
--- tests/Shared/Infrastructure/Testing/PHPUnit/TestCase.php
+++ tests/Shared/Infrastructure/Testing/PHPUnit/TestCase.php
@@ -8,14 +8,21 @@ use Mockery\Adapter\Phpunit\MockeryTestCase;
 use Mockery\MockInterface;
 use Teamleader\Discounts\Shared\Domain\Bus\Event\DomainEvent;
 use Teamleader\Discounts\Shared\Domain\Bus\Event\EventBus;
+use Teamleader\Discounts\Shared\Domain\Bus\Query\QueryBus;
 
 abstract class TestCase extends MockeryTestCase
 {
+    private QueryBus|MockInterface|null $queryBus = null;
     private EventBus|MockInterface|null $eventBus = null;
 
     protected function mock(string $class): MockInterface
     {
         return \Mockery::mock($class);
+    }
+
+    protected function queryBus(): QueryBus|MockInterface
+    {
+        return $this->queryBus ??= $this->mock(QueryBus::class);
     }
 
     protected function eventBus(): EventBus|MockInterface
